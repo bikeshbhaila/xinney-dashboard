902d48a80de3af4d7c7cf360df9fc2aa1d6c234a
bikeshbhaila <bhailabikesh1@gmail.com>
2021-02-04 21:48:30 +0545

    completed design of dashboard

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ $('#sidebarCollapse').on('click', function() {
     $('#sidebar').toggleClass('active');
     $('#body').toggleClass('active');
 });
+
+// Link active
+const linkColor = document.querySelectorAll('.nav__link')
+function colorLink() {
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
